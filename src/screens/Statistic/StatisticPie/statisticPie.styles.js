2424d35703496fc93c254a891300d5e7cd262ef3
Khôi Trần Duy <20521483@ms.uit.edu.vn>
2023-07-09 14:01:36 +0700

    lam nay lam kia

--- src/screens/Statistic/StatisticPie/statisticPie.styles.js
+++ src/screens/Statistic/StatisticPie/statisticPie.styles.js
@@ -1,8 +1,32 @@
 import { StyleSheet } from 'react-native';
+import theme from '../../../config/theme';
 
 const styles = StyleSheet.create({
-  example: {
-    color: '#000',
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.black,
+  },
+  statistic_container: {
+    height: '75%',
+    marginTop: '4%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  spending_type_container: {
+    height: '25%',
+    width: '100%',
+    backgroundColor: 'red',
+  },
+  select_month_container: {
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingVertical: '8%',
+  },
+  select_month_text: {
+    fontSize: theme.fontSizes.text_body,
+    color: theme.colors.white,
   },
 });
 
