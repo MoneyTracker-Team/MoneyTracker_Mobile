c1ed010c29879fa5c1cd6e38bf565c7741f63b6d
Hoàng Văn Phúc <20521760@gm.uit.edu.vn>
2023-07-12 22:48:11 +0700

    handle home screen

--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -1,14 +1,90 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, ImageBackground, Image, ScrollView } from 'react-native';
 import styles from './home.styles.js';
+import bgImg from '../../../assets/bg-img.png';
+import theme from '../../config/theme.js';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons.js';
+import formatTime from '../../utils/formatTime.js';
 
 const HomeScreen = ({ navigation }) => {
+  const [spends, setSpends] = useState([]);
+
+  useEffect(() => {
+    const fetchSpends = async () => {
+      const currDate = new Date();
+      const day = currDate.getDate();
+      const month = currDate.getMonth() + 1;
+      const year = currDate.getFullYear();
+
+      fetch(
+        `https://moneytrackerserver-production.up.railway.app/spends/in-date/6476fc3968a24efaacf90dc6?day=${day}&month=${month}&year=${year}`,
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          //* set data to state
+          setSpends(result?.data?.spends);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+    fetchSpends();
+  }, []);
+
   return (
-    <View>
-      <Text style={styles.example}>This is Home Screen</Text>
-      <Button title="Chi tiền" onPress={() => navigation.navigate('CreateSpending')} />
-      <Button title="Tạo kế hoạch chi tiêu" onPress={() => navigation.navigate('SpendSchedule')} />
-    </View>
+    <ImageBackground source={bgImg} style={{ height: '100%' }}>
+      <View style={{ padding: 20, display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
+        <TouchableOpacity style={styles.spendButton} onPress={() => navigation.navigate('CreateSpending')}>
+          <Text style={styles.spendButtonText}>Chi tiền</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ paddingHorizontal: 20, display: 'flex' }}>
+        <Text style={{ color: theme.colors.white, fontSize: theme.fontSizes.text_body }}>Tiền tiết kiệm</Text>
+        <Text style={{ color: theme.colors.white, fontSize: theme.fontSizes.headline_one, fontWeight: '500' }}>
+          3.500.000 VND
+        </Text>
+      </View>
+      {/* Bieu do */}
+      <View style={{ height: 230 }}></View>
+
+      <View style={{ display: 'flex', gap: 20 }}>
+        <View style={{ paddingHorizontal: 20 }}>
+          <Text style={{ color: theme.colors.white, fontSize: 20 }}> Chi tiêu hôm nay</Text>
+        </View>
+        {/* List spending in date */}
+        <ScrollView horizontal contentContainerStyle={styles.scrollViewSpend}>
+          {spends &&
+            spends.map((spend, index) => (
+              <View key={index} style={styles.spendItem}>
+                <Image
+                  source={{
+                    uri: spend?.image,
+                  }}
+                  style={styles.spendImage}
+                />
+                <View style={styles.spendContent}>
+                  <Text style={{ fontWeight: '800', fontSize: 20, marginTop: 6 }}>{spend.name}</Text>
+                  <Text style={{ flex: 1, fontSize: theme.fontSizes.text_body }}>{spend.note}</Text>
+                  <Text style={{ alignSelf: 'flex-end', marginRight: 10 }}>{formatTime(spend.dateTime)}</Text>
+                </View>
+              </View>
+            ))}
+        </ScrollView>
+      </View>
+
+      <View style={{ display: 'flex', alignItems: 'center', marginTop: 20 }}>
+        <TouchableOpacity style={styles.scheduleWrapBtn} onPress={() => navigation.navigate('SpendSchedule')}>
+          <View style={styles.cicleIconBtn}>
+            <MaterialIcons style={{ color: theme.colors.white, fontSize: 24 }} name={'double-arrow'} />
+          </View>
+          <View style={styles.scheduleBtn}>
+            <Text style={{ color: theme.colors.white, fontSize: 20, position: 'relative', top: -2 }}>
+              Xem kế hoạch chi tiêu
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
 
--- src/screens/Home/home.styles.js
+++ src/screens/Home/home.styles.js
@@ -2,8 +2,88 @@ import { StyleSheet } from 'react-native';
 import theme from '../../config/theme';
 
 const styles = StyleSheet.create({
-  example: {
-    color: theme.colors.dark_primary,
+  spendButton: {
+    width: 140,
+    height: 50,
+    backgroundColor: theme.colors.quaternary,
+    borderRadius: 10,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  spendButtonText: {
+    color: theme.colors.white,
+    fontSize: 20,
+  },
+
+  scrollViewSpend: {
+    paddingHorizontal: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+  },
+
+  // Spend Item
+
+  spendItem: {
+    backgroundColor: theme.colors.light_white,
+    opacity: 0.8,
+    width: 240,
+    height: 110,
+    padding: 4,
+    borderRadius: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+  },
+
+  spendImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    flexShrink: 0,
+  },
+
+  spendContent: {
+    flex: 1,
+  },
+
+  // Schedule Button
+  scheduleWrapBtn: {
+    height: 64,
+    width: 300,
+    position: 'relative',
+  },
+
+  cicleIconBtn: {
+    width: 58,
+    height: 58,
+    borderRadius: 50,
+    backgroundColor: theme.colors.quaternary,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 3,
+    left: 3,
+  },
+
+  scheduleBtn: {
+    paddingVertical: 20,
+    paddingLeft: 58,
+    height: 64,
+    width: 300,
+    display: 'flex',
+    flexDirection: 'row',
+    borderRadius: 50,
+    backgroundColor: theme.colors.quaternary,
+    borderColor: theme.colors.light_white,
+    borderWidth: 0.5,
+    opacity: 0.6,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
